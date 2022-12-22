fbe433ae87da5fac8a54556ed5f8bf37447df7e7
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-12-22 10:27:15 -0700

    Rename

--- client_real/src/globals.js
+++
@@ -1,9 +0,0 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? `${window.location.origin}/api`
-    : 'http://localhost:3001/api'
-
-export const STRIPE_URL =
-  process.env.NODE_ENV === 'production'
-    ? `${window.location.origin}/stripe/charge`
-    : 'http://localhost:3001/stripe/charge'
